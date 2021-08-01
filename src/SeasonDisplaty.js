d93369bd9cf9481249ac7308398d4f3b2e234d6d
Gurveen-Singhm <gurveensingh45@gmail.com>
2021-08-01 22:43:40 +0530

    Getting User Position

---
+++ src/SeasonDisplaty.js
@@ -0,0 +1,8 @@
+import React from "react";
+import reactDom from "react-dom";
+
+const seasonDisplay = () => {
+  return <div></div>;
+};
+
+export default seasonDisplay;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,40 @@
 import React from "react";
 import ReactDom from "react-dom";
+class App extends React.Component {
+  constructor(props) {
+    super(props);
 
-const App = () => {
-  return <div>Hello</div>;
-};
+    this.state = { lat: null, errMessage: "" };
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => {
+        this.setState({
+          lat: position.coords.latitude,
+        });
+      },
+      (err) => {
+        this.setState({ errMessage: err.message });
+      }
+    );
+  }
 
+  render() {
+    if(this.state.errMessage && !this.state.lat){
+    return (
+      <div>
+        Error: {this.state.errMessage}
+      </div>
+    );}
+
+    if(!this.state.errMessage && this.state.lat){
+      return (
+        <div>
+          Latitude: {this.state.lat}
+        </div>
+      );}
+
+      return(
+        <div>Loading!</div>
+      )
+  }
+}
 ReactDom.render(<App />, document.querySelector("#root"));
